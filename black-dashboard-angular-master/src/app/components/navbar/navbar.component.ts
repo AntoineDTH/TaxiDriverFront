2bf8097f02b4147955434744baa635af59d3a24f
Gryfikki <philippe_gense@hotmail.fr>
2021-08-29 12:27:40 +0200

    Commit autorite Admin NavBar Philippe

--- black-dashboard-angular-master/src/app/components/navbar/navbar.component.ts
+++ black-dashboard-angular-master/src/app/components/navbar/navbar.component.ts
@@ -43,6 +43,10 @@ export class NavbarComponent implements OnInit, OnDestroy {
     )
   }
 
+  autorAdmin(){
+    return this.appService.autorite==='admin'
+  }
+
   // function that adds color white/transparent to the navbar on resize (this is for the collapse)
    updateColor = () => {
    var navbar = document.getElementsByClassName('navbar')[0];
